f3e5424537a303bc14842f3b222d374d0f2f7ed6
ChristophRazek <52331031+ChristophRazek@users.noreply.github.com>
2024-01-12 09:08:34 +0100

    entferne "Today-Datum" aus Feedback email da verwirrend da keine prüfung ststtfindet, ob tatsächlich auf heutige email verwiesen wird

--- Email.py
+++ Email.py
@@ -41,8 +41,8 @@ def send_mail():
     outlook = win32.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
     mail.To = ";".join(receivers)
-    mail.Subject = f'Feedback zu AS Update: {today}'
-    mail.Body = f'Guten Morgen\n \nAnbei findest du die Anmerkungen zu den übermittelten Daten vom {today}.\nBei Fragen bitte um Rückmeldung.\n \nMit freundlichen Grüßen\n \nChristoph Razek, M.Sc.\nERP&Prozessmanager\nemea Handelsgesellschaft mbH\nWallnerstraße 3/22\nA-1010 Wien\nTel.:    +43 1 535 10 01 - 280\nFax:    +43 1 535 10 01 - 900'
+    mail.Subject = f'Feedback zu AS Update'
+    mail.Body = f'Guten Morgen\n \nAnbei findest du die Anmerkungen zu den übermittelten Daten.\nBei Fragen bitte um Rückmeldung.\n \nMit freundlichen Grüßen\n \nChristoph Razek, M.Sc.\nERP&Prozessmanager\nemea Handelsgesellschaft mbH\nWallnerstraße 3/22\nA-1010 Wien\nTel.:    +43 1 535 10 01 - 280\nFax:    +43 1 535 10 01 - 900'
     mail.Attachments.Add(r'S:\Schmid\AS_Feedback.xlsx')
 
     mail.Display()
